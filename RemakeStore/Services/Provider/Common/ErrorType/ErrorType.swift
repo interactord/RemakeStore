10c999aaac9b0455238ca107d49543e4128d0c38
Scott Moon <interactord@gmail.com>
2019-05-01 08:55:17 +0900

    feat: ErrorType 프로토콜 추가
    서비스 또는 어플리케이션 전반적으로 쓰이는 에러를 정의하기 위한 베이스에러를 추가하였습니다.

---
+++ RemakeStore/Services/Provider/Common/ErrorType/ErrorType.swift
@@ -0,0 +1,11 @@
+//
+// Created by Scott Moon on 2019-05-01.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import Foundation
+
+protocol ErrorType {
+	var raw: Error? { get }
+	var messageKey: String { get }
+}
