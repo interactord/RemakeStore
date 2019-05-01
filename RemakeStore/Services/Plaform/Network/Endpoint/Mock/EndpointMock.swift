7ef9f002e6d52bfa33dfe9b49835c038415d8b99
Scott Moon <interactord@gmail.com>
2019-05-02 08:07:08 +0900

    feat: Endpoint, Transformer 합성 구현체 추가
    - Endpoint 의 데이터를 URLRequest 형식으로 변환하는 기능을 추가하였습니다.

---
+++ RemakeStore/Services/Plaform/Network/Endpoint/Mock/EndpointMock.swift
@@ -0,0 +1,35 @@
+//
+// Created by Scott Moon on 2019-05-02.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import Foundation
+
+@testable import RemakeStore
+
+struct EndpointMock: Endpoint {
+
+  var baseURL: String
+  var path: String
+  var httpMethod: HTTPMethod
+  private(set) var queryParameters: [String: String]?
+  private(set) var headers: [String: String]?
+  var bodyParameters: [String: Any]?
+
+  init(
+    baseURL: String,
+    path: String,
+    httpMethod: HTTPMethod,
+    queryParameters: [String: String]? = nil,
+    headers: [String: String]? = nil,
+    bodyParameters: [String: Any]? = nil
+  ) {
+    self.baseURL = baseURL
+    self.path = path
+    self.httpMethod = httpMethod
+    self.queryParameters = queryParameters
+    self.headers = headers
+    self.bodyParameters = bodyParameters
+  }
+
+}
