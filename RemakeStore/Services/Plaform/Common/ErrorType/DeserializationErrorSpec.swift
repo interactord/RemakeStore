c36546cf979a48b45b351c977a050f32845886c2
Scott Moon <interactord@gmail.com>
2019-05-02 17:33:33 +0900

    feat:  DeserializationError 추가
    Data 에서 객체로 변환하는 시점에 에러가 발생할 경우, 사용하는 에러  타입입니다.

---
+++ RemakeStore/Services/Plaform/Common/ErrorType/DeserializationErrorSpec.swift
@@ -0,0 +1,33 @@
+//
+// Created by Scott Moon on 2019-05-01.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import XCTest
+
+@testable import RemakeStore
+
+class DeserializationErrorSpec: XCTestCase {
+
+	var sut: DeserializationError?
+
+	override func setUp() {
+		super.setUp()
+	}
+
+	override func tearDown() {
+		super.tearDown()
+		sut = nil
+	}
+
+	func test_inject_developer_message_init() {
+
+		let expectedKey = "error.global"
+		let expectedMessage = "test error"
+
+		sut = DeserializationError(developerMessage: expectedMessage)
+
+		XCTAssertNil(sut?.raw)
+		XCTAssertEqual(expectedKey, sut?.messageKey)
+	}
+}
