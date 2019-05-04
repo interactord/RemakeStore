e94b1a09b28d19b3bd376df2b9d1abfed10dd448
Scott Moon <interactord@gmail.com>
2019-05-05 01:19:57 +0900

    test: AnyRepository 테스트 코드 추가
    Decodable 객체에 대한 create, update, delete 테스트 코드를 추가하였습니다.

--- SCServiceKit/Repository/Implementation/AnyRepositorySpec.swift
+++ SCServiceKit/Repository/Implementation/AnyRepositorySpec.swift
@@ -70,6 +70,46 @@ class AnyRepositorySpec: XCTestCase {
 
   }
 
+  func test_inject_userDecodeMock_and_appClient_create_delete() {
+    let httpClientMock = MockBuilder.makeAppHttpClient()
+    let expectedCreateCall = XCTestExpectation(description: "test_inject_userDecodeMock_and_appClient_create")
+    let expectedDeleteCall = XCTestExpectation(description: "test_inject_userDecodeMock_and_appClient_create")
+
+    sutUserDecodable = AnyRepository<UserDecodableMock>(base: UserDecodableRepoMock(httpClient: httpClientMock))
+
+    guard
+      let sut = sutUserDecodable,
+      let disposeBag = disposeBag
+      else {
+      fatalError("Should be not nil")
+    }
+
+    sut.create(with: nil)
+      .subscribe(
+        onNext: { _ in
+          XCTFail("Should be no result")
+        },
+        onError: { err in
+          XCTAssertNotNil(err)
+          expectedCreateCall.fulfill()
+        }
+      ).disposed(by: disposeBag)
+
+    sut.delete(with: nil)
+      .subscribe(
+        onNext: { _ in
+          XCTFail("Should be no result")
+        },
+        onError: { err in
+          XCTAssertNotNil(err)
+          expectedDeleteCall.fulfill()
+        }
+      ).disposed(by: disposeBag)
+
+    wait(for: [expectedCreateCall, expectedDeleteCall], timeout: 5.0)
+
+  }
+
   func test_inject_userMock_and_appClient_update() {
     let httpClientMock = MockBuilder.makeAppHttpClient()
     let updateUserParameterMock = UpdateUserParameterMock(name: "test user", surname: "test surname")
@@ -83,7 +123,7 @@ class AnyRepositorySpec: XCTestCase {
       let sut = sutUser,
       let disposeBag = disposeBag
       else {
-        fatalError("Should be not nil")
+      fatalError("Should be not nil")
     }
 
     sut.update(with: updateUserParameterMock)
@@ -95,6 +135,40 @@ class AnyRepositorySpec: XCTestCase {
         case .value(let userMock):
           XCTAssertEqual(expectedUserMock.name, userMock.name)
           XCTAssertEqual(expectedUserMock.surname, userMock.surname)
+        }
+
+        expectedCall.fulfill()
+      }).disposed(by: disposeBag)
+
+    wait(for: [expectedCall], timeout: 5.0)
+
+  }
+
+  func test_inject_userDecodeMock_and_appClient_update() {
+    let httpClientMock = MockBuilder.makeAppHttpClient()
+    let updateUserParameterMock = UpdateUserParameterMock(name: "test user", surname: "test surname")
+
+    let expectedCall = XCTestExpectation(description: "call proceed")
+    let expectedUserMock = UserMock(name: "test name", surname: "test surname", age: "34")
+
+    sutUserDecodable = AnyRepository<UserDecodableMock>(base: UserDecodableRepoMock(httpClient: httpClientMock))
+
+    guard
+      let sut = sutUserDecodable,
+      let disposeBag = disposeBag
+      else {
+      fatalError("Should be not nil")
+    }
+
+    sut.update(with: updateUserParameterMock)
+      .subscribe(onNext: { result in
+
+        switch result {
+        case .noContent:
+          XCTFail("Should be no content")
+        case .value(let userMock):
+          XCTAssertEqual(expectedUserMock.name, userMock.args.name)
+          XCTAssertEqual(expectedUserMock.surname, userMock.args.surname)
         }
 
         expectedCall.fulfill()
