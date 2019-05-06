b6b70bd0a6a2337759aab38bceeb35d656e1a1e0
Scott Moon <interactord@gmail.com>
2019-05-07 04:16:16 +0900

    Fix: 코코아팟으로 대체, SCServiceKit 삭제
    - 기존 Carthage 에서 Cocoa pod 으로 라이브러리 관리 툴이 변경되었습니다.
    - travis-ci 의 환경설정 파일또한 코코아팟 라이브러리 인스톨로 변경되었습니다.
    - SCServiceKit 가 이제 코코아팟 라이브러리에 등록이 되어 프로젝트에서 제거 하였습니다.

--- SCServiceKit/Repository/NoRespositoryParameter.swift
+++
@@ -1,9 +0,0 @@
-//
-// Created by Scott Moon on 2019-05-03.
-// Copyright (c) 2019 Scott Moon. All rights reserved.
-//
-
-import Foundation
-
-struct NoRepositoryParameter: RepositoryParameter {
-}
