baf9519f90ba8edef5c109e510737ecd7ba1bc34
Scott Moon <interactord@gmail.com>
2019-05-07 16:38:51 +0900

    feat: Theme파트 추가
    Theme 파트는 디자인 테마인 Color, Font 등의 속성 값을 가지고 있는 객체를 추가하였습니다.

---
+++ RemakeStore/Define/Implementation/DefaultTheme+Font.swift
@@ -0,0 +1,14 @@
+//
+// Created by Scott Moon on 2019-05-07.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import UIKit
+
+extension DefaultTheme {
+	struct Font {
+		static var boldLarge: UIFont {
+			return .boldSystemFont(ofSize: 20)
+		}
+	}
+}
