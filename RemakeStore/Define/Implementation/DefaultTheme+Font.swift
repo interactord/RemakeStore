814632f4dd19c29bb3df3d01886e386203dc5328
Scott Moon <interactord@gmail.com>
2019-05-08 10:21:20 +0900

    faet: Default font 추가
    프로젝트에서 사용될 폰트를 지정하였습니다.

--- RemakeStore/Define/Implementation/DefaultTheme+Font.swift
+++ RemakeStore/Define/Implementation/DefaultTheme+Font.swift
@@ -7,8 +7,48 @@ import UIKit
 
 extension DefaultTheme {
 	struct Font {
-		static var boldLarge: UIFont {
-			return .boldSystemFont(ofSize: 20)
-		}
+		static var largeTitle: UIFont {
+      return .systemFont(ofSize: 34, weight: .regular)
+    }
+
+    static var title1: UIFont {
+      return .systemFont(ofSize: 28, weight: .regular)
+    }
+
+    static var title2: UIFont {
+      return .systemFont(ofSize: 22, weight: .regular)
+    }
+
+    static var title3: UIFont {
+      return .systemFont(ofSize: 20, weight: .regular)
+    }
+
+    static var headline: UIFont {
+      return .systemFont(ofSize: 17, weight: .semibold)
+    }
+
+    static var body: UIFont {
+      return .systemFont(ofSize: 17, weight: .regular)
+    }
+
+    static var callout: UIFont {
+      return .systemFont(ofSize: 16, weight: .regular)
+    }
+
+    static var subhead: UIFont {
+      return .systemFont(ofSize: 15, weight: .regular)
+    }
+
+    static var footnote: UIFont {
+      return .systemFont(ofSize: 13, weight: .regular)
+    }
+
+    static var caption1: UIFont {
+      return .systemFont(ofSize: 12, weight: .regular)
+    }
+
+    static var caption2: UIFont {
+      return .systemFont(ofSize: 11, weight: .regular)
+    }
 	}
 }
