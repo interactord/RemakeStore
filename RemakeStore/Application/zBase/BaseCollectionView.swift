e3ab1ccfea8602194945c77c36d075506b65f3af
Scott Moon <interactord@gmail.com>
2019-05-10 17:49:08 +0900

    refactor: BaseCollectionView 확장 기능 추가
    CollectionView에 Layout이 여러종류로 사용할 것을 고려하여 추가 파라미터를 추가했습니다.

--- RemakeStore/Application/zBase/BaseCollectionView.swift
+++ RemakeStore/Application/zBase/BaseCollectionView.swift
@@ -8,14 +8,31 @@ import UIKit
 import RxSwift
 
 class BaseCollectionView: UICollectionView {
+  
+  // MARK: Public
 
+  enum LayoutStyle {
+    case horizontalBetterSnapping
+    case `default`
+  }
+
+  // MARK: - Private
+  
   lazy private(set) public var disposeBag = DisposeBag()
 
   // MARK: - Initializing
 
-  convenience init() {
-    let layout = UICollectionViewFlowLayout()
-    self.init(frame: .zero, collectionViewLayout: layout)
+  convenience init(withLayoutStyle style: BaseCollectionView.LayoutStyle = .default) {
+
+    switch style {
+    case .default:
+      let layout = UICollectionViewFlowLayout()
+      self.init(frame: .zero, collectionViewLayout: layout)
+    case .horizontalBetterSnapping:
+      let layout = UICollectionViewFlowLayout()
+      self.init(frame: .zero, collectionViewLayout: layout)
+    }
+
     setupView()
     registerCell()
     setupDelegate()
