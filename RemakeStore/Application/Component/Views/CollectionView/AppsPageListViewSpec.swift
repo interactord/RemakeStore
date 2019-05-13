5957edc607aa726eaf9844c9ee4a3565c59a3d30
Scott Moon <interactord@gmail.com>
2019-05-14 06:03:56 +0900

    test: AppsPageListView 테스트 코드 추가

--- RemakeStore/Application/Component/Views/CollectionView/AppsPageListViewSpec.swift
+++ RemakeStore/Application/Component/Views/CollectionView/AppsPageListViewSpec.swift
@@ -23,4 +23,101 @@ class AppsPageListViewSpec: XCTestCase {
     sut = AppsPageListView()
     XCTAssertNotNil(sut)
   }
+
+  func test_dataSource_numberOfItemsInSection() {
+    let expectedSectionCount = 3
+    sut = AppsPageListView()
+
+    guard
+      let sut = sut,
+      let dataSource = sut.dataSource
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultCount = dataSource.collectionView(sut, numberOfItemsInSection: 0)
+
+    XCTAssertEqual(expectedSectionCount, resultCount)
+  }
+
+  func test_dataSource_cellForItemAt() {
+    sut = AppsPageListView()
+
+    guard
+      let sut = sut,
+      let dataSource = sut.dataSource
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultCell = dataSource.collectionView(sut, cellForItemAt: [0, 0])
+    XCTAssertNotNil(resultCell as? AppGroupCell)
+  }
+
+  func test_dataSource_viewForSupplementaryElementOfKind() {
+    sut = AppsPageListView()
+
+    guard
+      let sut = sut,
+      let dataSource = sut.dataSource
+      else {
+        fatalError("Should be not nil")
+    }
+
+    dataSource.collectionView(sut, cellForItemAt: [1, 3])
+    let resultHeaderView = dataSource.collectionView?(sut, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: .init(row: 0, section: 0))
+    XCTAssertNotNil(resultHeaderView as? AppsPageHeader)
+  }
+
+  func test_delegate_sizeForItemAt() {
+    let expectedHegith: CGFloat = 300
+
+    sut = AppsPageListView()
+
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultSize = delegate.collectionView?(sut, layout: layout, sizeForItemAt: [0, 0])
+
+    XCTAssertEqual(expectedHegith, resultSize?.height)
+  }
+
+  func test_delegate_referenceSizeForHeaderInSection() {
+    let expectedHegith: CGFloat = 300
+
+    sut = AppsPageListView()
+
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultSize = delegate.collectionView?(sut, layout: layout, referenceSizeForHeaderInSection: 1)
+    XCTAssertEqual(expectedHegith, resultSize?.height)
+  }
+
+  func test_delegate_insetForSectionAt() {
+    let expectedPadding = UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0)
+
+    sut = AppsPageListView()
+
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultPadding = delegate.collectionView?(sut, layout: layout, insetForSectionAt: 1)
+    XCTAssertEqual(expectedPadding, resultPadding)
+  }
 }
