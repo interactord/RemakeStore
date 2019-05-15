232563f9d0d54ec4556132f6593ae9c8a996b5b1
Scott Moon <interactord@gmail.com>
2019-05-16 00:42:58 +0900

    fix: FeedResultViewModel 추가

---
+++ RemakeStore/Application/Component/ViewModels/FeedResultVIewModel.swift
@@ -0,0 +1,40 @@
+//
+// Created by Scott Moon on 2019-05-16.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol FeedResultViewModelOutput {
+  var feedResult: Observable<FeedResult> { get }
+}
+
+protocol FeedResultViewModeling {
+  var outputs: FeedResultViewModelOutput { get }
+}
+
+protocol FeedResultViewModelBindable {
+  func bind(to viewMode: FeedResultViewModeling)
+}
+
+typealias FeedResultViewModelType =
+  FeedResultViewModelOutput & FeedResultViewModeling
+
+class FeedResultViewModel: FeedResultViewModelType {
+
+  // MARK: - Inputs & Outputs
+
+  var outputs: FeedResultViewModelOutput {
+    return self
+  }
+
+  // MARK: - Outputs
+
+  var feedResult: Observable<FeedResult>
+
+  init(withFeedResult feedResult: FeedResult) {
+    self.feedResult = Observable.just(feedResult).observeOn(MainScheduler.asyncInstance)
+  }
+}
