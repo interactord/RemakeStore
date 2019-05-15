b2259b07864116644e5c5af3556fed8a2f0f0f99
Scott Moon <interactord@gmail.com>
2019-05-15 23:53:34 +0900

    test: AppsViewModel 테스트 코드 추가

--- RemakeStore/Application/Scene/Apps/AppsViewModelSpec.swift
+++ RemakeStore/Application/Scene/Apps/AppsViewModelSpec.swift
@@ -6,24 +6,25 @@
 import XCTest
 
 import RxSwift
-import RxTest
+import RxBlocking
 
 @testable import RemakeStore
 
 class AppsViewModelSpec: XCTestCase {
   var sut: AppsViewModel!
+  let server = StubServer()
   var disposbag = DisposeBag()
-  var scheduler = TestScheduler(initialClock: 0)
 
   override func setUp() {
     super.setUp()
+    server.setup()
     disposbag = DisposeBag()
-    scheduler = TestScheduler(initialClock: 0)
   }
 
   override func tearDown() {
     super.tearDown()
     sut = nil
+    server.tearDown()
   }
 
   func test_init() {
@@ -33,13 +34,26 @@ class AppsViewModelSpec: XCTestCase {
   }
 
   func test_fetchApps() {
+    let expectedCallEvent = XCTestExpectation(description: "called event")
+
+    sut = makeSUT()
+
+    sut.inputs.fetchApps.onNext(Void())
+    sut.outputs.appGroups
+      .subscribe(onNext: { apps in
+        XCTAssertNotNil(apps)
+        expectedCallEvent.fulfill()
+      }).disposed(by: disposbag)
+
+    wait(for: [expectedCallEvent], timeout: 10.0)
 
   }
 
   func makeSUT() -> AppsViewModel {
+
     var serviceDIContainer = ServiceDIContainer()
     let service = serviceDIContainer.service
-    let rssRepository = RssRepository(httpClient: service.httpClient, baseURL: "http://localHost:7000")
+    let rssRepository = RssRepository(httpClient: service.httpClient, baseURL: "http://localhost:7000")
 
     return AppsViewModel(with: service, rssRepository: rssRepository)
   }
