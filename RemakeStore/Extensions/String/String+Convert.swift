0ac58f2216e1db922eaf6b5cbc90061ef845969d
Scott Moon <interactord@gmail.com>
2019-05-17 03:31:35 +0900

    feat: extension UIColor, String -> RGB 컬러 변환 메소드 추가

--- RemakeStore/Extensions/String/String+Convert.swift
+++ RemakeStore/Extensions/String/String+Convert.swift
@@ -3,10 +3,33 @@
 // Copyright (c) 2019 Scott Moon. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension String {
   func toInt() -> Int? {
     return Int(self)
   }
+
+  func toHexColor() -> UIColor {
+    var hexString = ""
+
+    if self.hasPrefix("#") {
+      hexString = String(self[self.index(after: self.startIndex)...])
+    }
+
+    guard let hexVal = Int(hexString, radix: 16) else {
+      return UIColor()
+    }
+
+    switch hexString.count {
+    case 3:
+      return UIColor(hex3: hexVal, alpha: 1.0) ?? UIColor()
+    case 6:
+      return UIColor(hex6: hexVal, alpha: 1.0) ?? UIColor()
+    default:
+      return UIColor()
+    }
+
+  }
+
 }
