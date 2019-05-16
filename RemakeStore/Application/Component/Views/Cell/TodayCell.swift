be9e682c5f1c33997222bf8cb66ced0681fb54ac
Scott Moon <interactord@gmail.com>
2019-05-17 02:00:32 +0900

    feat: TodayCell 프로토타입 추가

--- RemakeStore/Application/Component/Views/Cell/TodayCell.swift
+++ RemakeStore/Application/Component/Views/Cell/TodayCell.swift
@@ -5,7 +5,82 @@
 
 import UIKit
 
+import SCUIBuildKit
+import SCLayoutKit
+
 class TodayCell: BaseTodayCell {
+
+  // MARK: Public
+
+  var topConstraint: NSLayoutConstraint!
+
+  // MARK: - Private
+
+  private lazy var categoryLabel: UILabel = {
+    return LabelBuilder()
+      .setText("CATEGORY LABEL")
+      .setFont(DefaultTheme.Font.title3)
+      .build()
+  }()
+
+  private lazy var titleLabel: UILabel = {
+    return LabelBuilder()
+      .setText("TITLE LABEL")
+      .setFont(DefaultTheme.Font.title1)
+      .build()
+  }()
+
+  private lazy var imageView: UIImageView = {
+    return ImageViewBuilder()
+      .setImage(#imageLiteral(resourceName: "Garden"))
+      .setContentMode(.scaleAspectFill)
+      .setClipToBounds(true)
+      .build()
+  }()
+
+  private lazy var imageContainerView: UIView = {
+    let view = UIView()
+    view.addSubview(imageView)
+    imageView.centerInSuperview(size: .init(width: 240, height: 240))
+    return view
+  }()
+
+  private lazy var descriptionLabel: UILabel = {
+    return LabelBuilder()
+      .setText("All the tools and apps your need to intelligently organize your life the right way.")
+      .setFont(DefaultTheme.Font.callout)
+      .setNumberOfLines(3)
+      .build()
+  }()
+
+  private lazy var stackView: UIStackView = {
+    return StackViewBuilder(
+      arrangedSubViews: [
+        categoryLabel,
+        titleLabel,
+        imageContainerView,
+        descriptionLabel
+      ])
+      .setAxis(.vertical)
+      .setSpacing(8)
+      .build()
+  }()
+
+  override func setupSubView() {
+    super.setupSubView()
+    addSubview(stackView)
+  }
+
+  override func setupConstant() {
+    super.setupConstant()
+    stackView
+      .setLeadingAnchor(leadingAnchor, padding: 24)
+      .setBottomAnchor(bottomAnchor, padding: 24)
+      .setTrailingAnchor(trailingAnchor, padding: 24)
+
+    topConstraint = stackView.topAnchor.constraint(equalTo: topAnchor, constant: 24)
+    topConstraint.isActive = true
+  }
 }
 
 extension TodayCell: CellContentClassIdentifiable {
