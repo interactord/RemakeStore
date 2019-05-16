d412f0b62039e073be8206272fb389d1dee81aab
Scott Moon <interactord@gmail.com>
2019-05-16 18:17:09 +0900

    feat: TodayListView 프로토타입 추가

--- RemakeStore/Application/Component/Views/CollectionView/TodayListView.swift
+++ RemakeStore/Application/Component/Views/CollectionView/TodayListView.swift
@@ -7,8 +7,72 @@ import UIKit
 
 class TodayListView: BaseCollectionView {
 
-  override func setupView() {
-    super.setupView()
-    backgroundColor = DefaultTheme.Color.secondaryColor
-  }
+	// MARK: - Private
+
+	private let contentCellHeight: CGFloat = 466
+	private let contentCellPadding: CGFloat = 48
+	private let contentCellMargin: UIEdgeInsets = .init(top: 10, left: 0, bottom: 32, right: 0)
+	private let cellLineSpacing: CGFloat = 32
+	private let headerCellHeight: CGFloat = 85
+
+	override func registerCell() {
+		super.registerCell()
+		register(cellType: TodayCell.self)
+		register(cellType: TodayMultipleAppCell.self)
+		register(kind: UICollectionView.elementKindSectionHeader, reusableViewType: TodayHeader.self)
+	}
+
+	override func setupDelegate() {
+		super.setupDelegate()
+
+		self.delegate = self
+		self.dataSource = self
+	}
+
+	override func setupView() {
+		super.setupView()
+		backgroundColor = DefaultTheme.Color.secondaryColor
+	}
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension TodayListView: UICollectionViewDataSource {
+	public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 10
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell: TodayCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+		cell.backgroundColor = .red
+		return cell
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+		let header: TodayHeader = collectionView.dequeueReusableCell(kind: kind, indexPath: indexPath)
+		header.backgroundColor = .yellow
+		return header
+	}
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension TodayListView: UICollectionViewDelegateFlowLayout {
+
+	public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+		return  .init(width: frame.width, height: headerCellHeight)
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return .init(width: frame.width - contentCellPadding, height: contentCellHeight)
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+		return cellLineSpacing
+	}
+
+	public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return contentCellMargin
+	}
+
 }
--- RemakeStore/Application/Component/Views/CollectionView/TodayListViewSpec.swift
+++ RemakeStore/Application/Component/Views/CollectionView/TodayListViewSpec.swift
@@ -4,25 +4,142 @@
 //
 
 import XCTest
+import UIKit
 
 @testable import RemakeStore
 
 class TodayListViewSpec: XCTestCase {
 
-	var sut: TodayListView?
+  var sut: TodayListView?
 
-	override func setUp() {
-		super.setUp()
-	}
+  override func setUp() {
+    super.setUp()
+  }
 
-	override func tearDown() {
-		super.tearDown()
-		sut = nil
-	}
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+  }
 
-	func test_init() {
-		sut = TodayListView()
+  func test_init() {
+    sut = TodayListView()
 
-		XCTAssertNotNil(sut)
-	}
+    XCTAssertNotNil(sut)
+  }
+
+  func test_dataSource_numberOfItemsInSection() {
+    let expectedCount = 10
+
+    sut = TodayListView()
+
+    guard
+      let sut = sut,
+      let dataSource = sut.dataSource
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultCount = dataSource.collectionView(sut, numberOfItemsInSection: 0)
+
+    XCTAssertEqual(expectedCount, resultCount)
+  }
+
+  func test_dataSource_cellForItemAt() {
+    sut = TodayListView()
+    guard
+      let sut = sut,
+      let dataSource = sut.dataSource
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultCell = dataSource.collectionView(sut, cellForItemAt: [0, 0])
+
+    XCTAssertNotNil(resultCell as? TodayCell)
+  }
+
+  func test_dataSource_viewForSupplementaryElementOfKind() {
+
+    sut = TodayListView()
+    guard
+      let sut = sut,
+      let dataSource = sut.dataSource
+      else {
+        fatalError("Should be not nil")
+    }
+
+    _ = dataSource.collectionView(sut, cellForItemAt: [0, 3])
+    let resultHeader = dataSource.collectionView?(sut, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: [0, 3])
+
+    XCTAssertNotNil(resultHeader as? TodayHeader)
+  }
+
+  func test_delegate_referenceSizeForHeaderInSection() {
+    let expectedHeight: CGFloat = 85
+
+    sut = TodayListView()
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultSize = delegate.collectionView?(sut, layout: layout, referenceSizeForHeaderInSection: 0)
+
+    XCTAssertEqual(expectedHeight, resultSize?.height)
+  }
+
+  func test_delegate_sizeForItemAt() {
+    let expectedHeight: CGFloat = 466
+
+    sut = TodayListView()
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultSize = delegate.collectionView?(sut, layout: layout, sizeForItemAt: [0, 0])
+
+    XCTAssertEqual(expectedHeight, resultSize?.height)
+  }
+
+  func test_delegate_minimumInteritemSpacingForSectionAt() {
+
+    let expectedLineSpacing: CGFloat = 32
+
+    sut = TodayListView()
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultLineSpacing = delegate.collectionView?(sut, layout: layout, minimumInteritemSpacingForSectionAt: 0)
+
+    XCTAssertEqual(expectedLineSpacing, resultLineSpacing)
+  }
+
+  func test_delegate_insetForSectionAt() {
+    let expectedMargin = UIEdgeInsets(top: 10, left: 0, bottom: 32, right: 0)
+
+    sut = TodayListView()
+    guard
+      let sut = sut,
+      let delegate = sut.delegate as? UICollectionViewDelegateFlowLayout,
+      let layout = sut.collectionViewLayout as? UICollectionViewFlowLayout
+      else {
+        fatalError("Should be not nil")
+    }
+
+    let resultMargin = delegate.collectionView?(sut, layout: layout, insetForSectionAt: 0)
+
+    XCTAssertEqual(expectedMargin, resultMargin)
+  }
 }
