bb65432f348f88952783f1d13fdd980b392963a1
Scott Moon <interactord@gmail.com>
2019-05-18 01:11:42 +0900

    feat: MultipleAppCell 추가
    해당 셀은 Today에서 앱 정보를 보여주는 Cell입니다.

---
+++ RemakeStore/Application/Component/Views/Cell/MultipleAppCell.swift
@@ -0,0 +1,12 @@
+//
+// Created by Scott Moon on 2019-05-17.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import UIKit
+
+class MultipleAppCell: BaseCollectionViewCell {
+}
+
+extension MultipleAppCell: CellContentClassIdentifiable {
+}
--- RemakeStore/Application/Component/Views/Cell/TodayMultipleAppCell.swift
+++ RemakeStore/Application/Component/Views/Cell/TodayMultipleAppCell.swift
@@ -5,7 +5,40 @@
 
 import UIKit
 
+import SCLayoutKit
+import SCUIBuildKit
+
 class TodayMultipleAppCell: BaseTodayCell {
+
+	private lazy var todayMultipleAppListView: TodayMultipleAppListView = {
+		let listView = TodayMultipleAppListView()
+		listView.viewMode(to: .small)
+		return listView
+	}()
+
+	private lazy var stackView: UIStackView = {
+		return StackViewBuilder(
+			arrangedSubViews: [
+				categoryLabel,
+				titleLabel,
+				todayMultipleAppListView
+			])
+			.setAxis(.vertical)
+			.setSpacing(12)
+			.build()
+	}()
+
+	override func setupSubView() {
+		super.setupSubView()
+		addSubview(stackView)
+	}
+
+	override func setupConstant() {
+		super.setupConstant()
+		stackView.fillSuperView(
+			padding: .init(top: 24, left: 24, bottom: 24, right: 24)
+		)
+	}
 }
 
 extension TodayMultipleAppCell: CellContentClassIdentifiable {
