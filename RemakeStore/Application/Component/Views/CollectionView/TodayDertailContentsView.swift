fb01c5a8d6c7ada0803678431e5887588f0f5f1b
Scott Moon <interactord@gmail.com>
2019-05-19 05:21:04 +0900

    feat: TodayDetail 장면 추가

---
+++ RemakeStore/Application/Component/Views/CollectionView/TodayDertailContentsView.swift
@@ -0,0 +1,13 @@
+//
+// Created by Scott Moon on 2019-05-19.
+// Copyright (c) 2019 Scott Moon. All rights reserved.
+//
+
+import UIKit
+
+class TodayDetailContentsView: BaseCollectionView {
+	override func setupView() {
+		super.setupView()
+		backgroundColor = .red
+	}
+}
