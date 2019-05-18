8ff0da01c684c69b658650828fcb4fbbd1850848
Scott Moon <interactord@gmail.com>
2019-05-18 19:41:59 +0900

    fix: TodayItem 구조 변경

--- RemakeStore/Entity/TodayItem.swift
+++ RemakeStore/Entity/TodayItem.swift
@@ -8,11 +8,10 @@ import UIKit
 struct TodayItem: Decodable {
   let category: String
   let title: String
-  let imageUrl: String
-  let description: String
-  let theme: String
+  let imageUrl: String?
+  let description: String?
+  let theme: String?
   let cellType: String
-  let rssUrl: String
-  let lookupUrl: String
-  let appGroup: AppsGroup?
+  let rss: AppsGroup?
+  let lookup: Lookup?
 }
