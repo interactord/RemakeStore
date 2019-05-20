2cf2bcffa6fa6ee6df8c06276373691e1bea60b0
Scott Moon <interactord@gmail.com>
2019-05-21 06:50:12 +0900

    feat: animation 세분화

--- RemakeStore/Extensions/UIView/UIView+Animation.swift
+++ RemakeStore/Extensions/UIView/UIView+Animation.swift
@@ -6,34 +6,54 @@
 import UIKit
 
 extension UIView {
-	func animated(
-		duration: TimeInterval = 0.7,
-		delay: TimeInterval = 0.7,
-		usingSpringWithDamping: CGFloat = 0.7,
-		initialSpringVelocity: CGFloat = 0.7,
-		options: AnimationOptions,
-		animations: @escaping () -> Void
-	) {
-		UIView.animate(
-			withDuration: duration,
-			delay: delay,
-			usingSpringWithDamping: usingSpringWithDamping,
-			initialSpringVelocity: initialSpringVelocity,
-			options: options,
-			animations: animations
-		)
-	}
+  func defaultAnimated(
+    duration: TimeInterval = 0.7,
+    delay: TimeInterval = 0.0,
+    usingSpringWithDamping: CGFloat = 0.7,
+    initialSpringVelocity: CGFloat = 0.7,
+    options: AnimationOptions = .curveEaseOut,
+    animations: @escaping () -> Void,
+    completion: @escaping (Bool) -> Void
+  ) {
+    UIView.animate(
+      withDuration: duration,
+      delay: delay,
+      usingSpringWithDamping: usingSpringWithDamping,
+      initialSpringVelocity: initialSpringVelocity,
+      options: options,
+      animations: animations,
+      completion: completion
+    )
+  }
 
-	func animated(transform: CGAffineTransform) {
-		UIView.animate(
-			withDuration: 0.5,
-			delay: 0,
-			usingSpringWithDamping: 1,
-			initialSpringVelocity: 1,
-			options: .curveEaseOut,
-			animations: {
-				self.transform = transform
-			}
-		)
-	}
+  func defaultAnimated(
+    duration: TimeInterval = 0.7,
+    delay: TimeInterval = 0.0,
+    usingSpringWithDamping: CGFloat = 0.7,
+    initialSpringVelocity: CGFloat = 0.7,
+    options: AnimationOptions = .curveEaseOut,
+    animations: @escaping () -> Void
+  ) {
+    UIView.animate(
+      withDuration: duration,
+      delay: delay,
+      usingSpringWithDamping: usingSpringWithDamping,
+      initialSpringVelocity: initialSpringVelocity,
+      options: options,
+      animations: animations
+    )
+  }
+
+  func animated(transform: CGAffineTransform) {
+    UIView.animate(
+      withDuration: 0.5,
+      delay: 0,
+      usingSpringWithDamping: 1,
+      initialSpringVelocity: 1,
+      options: .curveEaseOut,
+      animations: {
+        self.transform = transform
+      }
+    )
+  }
 }
