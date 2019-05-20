3b0e8f1c5986fbb60e1342314833e28564800f8b
Scott Moon <interactord@gmail.com>
2019-05-20 23:05:17 +0900

    feat: TodayDetailViewInfo
     todayController -> todayDetailController로 필요한 정보를 주입 시키는 용도로 만들었습니다.

---
+++ RemakeStore/Entity/TodayDetailViewInfo.swift
@@ -0,0 +1,15 @@
+//
+//  TodayDetailViewInfo.swift
+//  RemakeStore
+//
+//  Created by Scott Moon on 20/05/2019.
+//  Copyright © 2019 Scott Moon. All rights reserved.
+//
+
+import UIKit
+
+struct TodayDetailViewInfo {
+  let indexItem: Int
+  var todayItemVIewModel: TodayItemViewModeling
+  var startingFrame: CGRect
+}
