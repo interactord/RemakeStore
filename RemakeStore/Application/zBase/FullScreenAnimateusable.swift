9f2f3c3e355edc7bf25bc3eaaa514d148f41e9e9
Scott Moon <interactord@gmail.com>
2019-05-21 06:51:20 +0900

    feat: 프로토콜 추가
    해당 프로토콜은 풀스크린 에니메이션 기능을 사용하기 위한 규칙이 담겨 있는 프로토콜입니다.

---
+++ RemakeStore/Application/zBase/FullScreenAnimateusable.swift
@@ -0,0 +1,30 @@
+//
+//  FullScreenAnimateusable.swift
+//  RemakeStore
+//
+//  Created by Scott Moon on 21/05/2019.
+//  Copyright © 2019 Scott Moon. All rights reserved.
+//
+
+import UIKit
+import SCLayoutKit
+
+enum ScreenStatus {
+  case thumbnail
+  case fullScreen
+}
+
+protocol FullScreenAnimatable where Self: UIViewController {
+  var targetFullScreenController: BaseFullScreenAnimatable? { get }
+  var fullScreenStatus: ScreenStatus { get }
+  func setupFullscreenView(withTodayDetailInfo info: FullScreenAnimatedInfo) -> BaseFullScreenAnimatable?
+  func startFullScreenAnimation()
+}
+
+protocol BaseFullScreenAnimatable where Self: UIViewController {
+  var baseView: UIView { get }
+  var beginningAnimateConstraints: AnchoredConstraints? { get set }
+  var finishedAnimatedConstraints: AnchoredConstraints? { get }
+  func setupFullScreenLayout(startingFrame: CGRect)
+  func startFullScreenAnimation()
+}
