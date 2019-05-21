bb2127ac902dcf6bb06932766f47bc2ee73e3706
Scott Moon <interactord@gmail.com>
2019-05-21 11:26:12 +0900

    feat: 셀 드래그 시, 스케일 계산 확장 메소드 추가

--- RemakeStore/Extensions/CGFloat/CGFloat+Divider.swift
+++ RemakeStore/Extensions/CGFloat/CGFloat+Divider.swift
@@ -10,4 +10,22 @@ extension CGFloat {
     let bounce = self - (grid - 1) * padding - 2 * lineSpacing
     return  bounce / grid
   }
+
+  func convertDragScaleTranform(withOffset offset: CGFloat) -> CGAffineTransform? {
+    if self <= 0 {
+      return nil
+    }
+
+    let trueOffset = self - offset
+    var scale: CGFloat = 1 - trueOffset / 1_000
+    scale = CGFloat.minMax(minValue: 1, maxValue: 0.5, tagetValue: scale)
+    return .init(scaleX: scale, y: scale)
+  }
+
+  static func minMax(minValue: CGFloat, maxValue: CGFloat, tagetValue: CGFloat) -> CGFloat {
+    var value = tagetValue
+    value = Swift.min(minValue, value)
+    value = Swift.max(maxValue, value)
+    return value
+  }
 }
